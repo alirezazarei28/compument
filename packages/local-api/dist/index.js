d27b42e68301337c0414445324e742c710c8b6ae
Alireza <alirezazarei28@gmail.com>
2022-01-16 17:40:06 +0330

    commander added to project for parsing terminal commands

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    console.log("server is listening");
+exports.serve = void 0;
+const serve = (port, filename, dir) => {
+    console.log("serving on port: \n", port, "serving fetching from \n  ", filename, "file is in \n", dir);
 };
+exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,10 @@
-export default () => {
-  console.log("server is listening");
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log(
+    "serving on port: \n",
+    port,
+    "serving fetching from \n  ",
+    filename,
+    "file is in \n",
+    dir
+  );
 };
